31887782b7e31f4343bcc7aacf1ec0854f49933b
Napat Srichantapong <>
2022-04-04 02:16:38 +0700

    Add firebase and newer version kotlin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
 
-void main() {
   runApp(const MyApp());
 }
 
