7a98dce2bcdeeb463af5ffdd88c316c4fb4588e8
Napat Srichantapong <>
2022-04-04 02:19:17 +0700

    test code magic

--- lib/main.dart
+++ lib/main.dart
@@ -107,6 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
+            const Text('Hello world'),
           ],
         ),
       ),
